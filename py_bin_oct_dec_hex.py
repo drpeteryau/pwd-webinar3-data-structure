610a4d875da61cde8d71c2bd77482c4bb9ac24a8
drpeteryau <97417362+drpeteryau@users.noreply.github.com>
2022-04-30 22:53:12 +0800

    Create py_bin_oct_dec_hex.py

---
+++ py_bin_oct_dec_hex.py
@@ -0,0 +1,12 @@
+#Binary, Decimal, Octal, Hexadecimal
+#About Network IP address: https://en.wikipedia.org/wiki/IP_address
+#Analog to Digital: https://screaminfx.com/tech/analog-verse-digital-guitar-pedals.htm
+#Readability: https://www.zytrax.com/tech/protocols/hex.html
+
+# Python program to convert decimal into other number systems
+decimal = 172
+
+print("Decimal (source) value: ", decimal)
+print("converted to Binary ", bin(decimal))
+print("converted to Octal: ", oct(decimal))
+print("converted to Hexadecimal: ", hex(decimal))
